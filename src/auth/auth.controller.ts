13b95a914fa40f97743105a7aa7c112306a544e0
jyfernandez <jydfernandez@gmail.com>
2021-07-28 16:16:43 +0800

    Signing a jwt token in sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
   @Post('/signin')
   async signIn(
     @Body() authCredentialsDTO: AuthCredentialsDTO,
-  ): Promise<string> {
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDTO);
   }
 }
